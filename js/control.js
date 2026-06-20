1444f7c9e17e2e30034fc617ba5eace9f0897c67
trinhtriet4-maker <trinhtriet4@gmail.com>
2026-06-20 13:56:37 +0700

    Update control.js

--- js/control.js
+++ js/control.js
@@ -1,1 +1,25 @@
+let score = 7.2;
 
+if (score >= 8.5) {
+    console.log("Xếp loại: Giỏi");
+} else if (score >= 6.5) {
+    console.log("Xếp loại: Khá");
+} else if (score >= 5) {
+    console.log("Xếp loại: Trung bình");
+} else {
+    console.log("Xếp loại: Yếu");
+}
+
+console.log("In các số từ 1 đến 5 bằng vòng for:");
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+let total = 0;
+
+for (let i = 1; i <= 5; i++) {
+    total += i;
+}
+
+console.log("Tổng =", total);
